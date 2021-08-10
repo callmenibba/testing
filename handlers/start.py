0ca1bd00f96eae6da5f15921691f5883b84967bd
callmenibba <84298992+callmenibba@users.noreply.github.com>
2021-08-10 07:30:02 -0700

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,72 +1,109 @@
 from pyrogram import Client, filters
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from handlers.play import cb_admin_check
 from config import BOT_USERNAME
 
 
-@Client.on_message(filters.command(["start", "start@GroupMusicPlayBot"]) & filters.private & ~filters.channel)
+@Client.on_message(filters.command(["start", f"start@{BOT_USERNAME}"]) & filters.private & ~filters.channel)
 async def start(_, message: Message):
     await message.reply_text(
-        text="**Hello 👋🏻 {}!**\n\nI **Can Play Music In Voice Chats of Telegram Groups.**I Have A **lot of cool feature that will amaze You!**\n\n**Click /cmdlist For More Help On My Usage ❤**".format(message.from_user.mention),
+        text="**Hey {}!**\n\nI can play music In Voice Chats of Telegram Groups.I Have A lot of cool feature that will amaze You!\n\nHit /help to know more functions of mine.".format(message.from_user.mention),
         reply_markup=InlineKeyboardMarkup(
-            [[
-            InlineKeyboardButton("➕ Add To Your Group ➕", url="https://t.me/GroupMuSicPlayBot?startgroup=true")
-            ],[
-            InlineKeyboardButton("💬 Group", url="https://t.me/MusicBotSupports"),
-            InlineKeyboardButton("Channel 🔊", url="https://t.me/GroupMusicXNews")
-            ],[
-            InlineKeyboardButton("Commands 🛠", url="https://telegra.ph/Music-Bot-05-07")
-            ]]
+            [
+                [
+                    InlineKeyboardButton(
+                        "➕ Add me to your Group", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
+                ],
+                [
+                    InlineKeyboardButton(
+                        "🆘 Support", url=f"https://t.me/AnteikuUnion"), 
+                    InlineKeyboardButton(
+                        "Updates 🔉", url=f"https://t.me/SuzuyaUpdates"),
+                ],
+            ],
         ),
+        reply_to_message_id=message.message_id,
         disable_web_page_preview=True
     )
         
-@Client.on_message(filters.command(["start", "start@GroupMusicPlayBot"]) & ~filters.private & ~filters.channel)
+@Client.on_message(filters.command(["start", f"start@{BOT_USERNAME}"]) & ~filters.private & ~filters.channel)
 async def gstart(_, message: Message):
     await message.reply_text(
-        text="**Music Bot Is Online ✅**",
+        text="Music Bot is still Awake!!",
         reply_markup=InlineKeyboardMarkup(
-            [[
-            InlineKeyboardButton(text="🎙️ Support Group 🎙️", url="https://t.me/MusicBotSupports")
-            ]]
-        )
+            [
+                [
+                    InlineKeyboardButton(
+                        text="🆘 Support", url="https://t.me/AnteikuUnion"
+                    ),
+                ],
+                [    
+                    InlineKeyboardButton(
+                        text="Search YT 🔎", switch_inline_query_current_chat=""
+                    ),
+                    InlineKeyboardButton(
+                        text="Delete", callback_data="del"
+                    ),
+                ],
+            ],
+        ),
     )
 
 
-@Client.on_message(filters.command(["cmdlist", "start@GroupMusicPlayBot"]) & filters.private & ~filters.channel)
-async def cmdlist(_, message: Message):
+
+@Client.on_message(filters.command(["help", "help@AnteikuMusicRobot"]) & filters.private & ~filters.channel)
+async def help(_, message: Message):
     await message.reply_text(
-        text="""**Group Music Bot : Help Menu**
-
-__× First Add Me To Your Group..
-× Promote Me As Admin In Your Group With All Permission..__
-
-**🏷 Common Commands.**
-
-• `/play` - Song Name : __Plays Via Youtube__
-• `/dplay` - Song Name : __Play Via Deezer__
-• `/splay` - Song Name : __Play Via Jio Saavn__
-• `/playlist` - __Show now playing list__
-• `/current` - __Show now playing__
-
-• `/song` - Song Name : __Get The Song From YouTube__
-• `/vid` - Video Name : __Get The Video From YouTube__
-• `/deezer` - song name : __download songs you want quickly via deezer__
-• `/saavn` - song name : __download songs you want quickly via saavn__
-• `/search` - YouTube Title : __(Get YouTube Search Query)__
-
-**🏷 Group Admin Commands.**
-
-• `/skip` : __Skips Music__
-• `/pause` : __Pause Playing Music__
-• `/resume` : __Resume Playing Music__
-• `/end` : __Stops playing Music__
-• `/reload` : __Reloads Admin List__
-• `/userbotjoin` : __Assistant Joins The Group__
-• `/userbotleave` : __Assistant Leaves From The Group.__""",
+        text="""**Anteiku Music : Help Menu**
+╒════**「 Setup Guide 」**
+├1.Promote bot as admin.
+├ (with **Manage Voice Chats** rights)
+├2.Turn on voice chat.
+├3.Run `/play ``[song name]` in your chat.
+├4.If userbot joins the chat, enjoy music.
+├  If not, add @AnteikuMusicBot manually.
+╘══════════════════
+╒════**「 Music Player 」**
+├ `/play` - Song Name : Plays Via Youtube.
+├ `/dplay` - Song Name : Play Via Deezer.
+├ `/splay` - Song Name : Play Via Jio Saavn.
+├ `/playlist` - Shows playing song list.
+├ `/current` - Shows now playing song .
+├
+╞══**「 Extras 」**
+├ `/song` : Download song from YouTube.
+├ `/vsong` : Download video from YouTube.
+├ `/vid` : Same as `/vsong`.
+├ `/deezer` : Download songs via deezer.
+├ `/saavn` : Download songs via saavn.
+├ `/search` : Get YouTube search query.
+╘══════════════════
+╒════**「 Admin Commands 」**
+├ `/skip` : Skips Music.
+├ `/pause` : Pause Playing Music.
+├ `/resume` : Resume Playing Music.
+├ `/end` : Stops playing Music.
+├ `/reload` : Reloads Admin List.
+├ `/userbotjoin` : Assistant Joins The Group.
+├ `/userbotleave` : Assistant Leaves From The Group.
+╘══════════════════""",
         reply_markup=InlineKeyboardMarkup(
-              [[
-              InlineKeyboardButton(text="🎙️ Support Group 🎙️", url="https://t.me/MusicBotSupports")
-              ]]
-          )
-      )
+              [
+                  [
+                      InlineKeyboardButton(
+                          text="🆘 Support",
+                          url="https://t.me/AnteikuUnion"),
+                  ],
+                  [
+                      InlineKeyboardButton(
+                          text="➕ Add me to your Group", 
+                          url=f"https://t.me/{BOT_USERNAME}?startgroup=true"),
+                  ],
+              ],
+            ),
+        )
+
+@Client.on_callback_query(filters.regex(pattern=r"del"))
+async def dell_cb(_, cb):
+    await cb.answer()
+    await cb.message.delete()
