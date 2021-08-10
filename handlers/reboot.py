75f37bbb2fdf84ca2799964d39eea93452189e18
callmenibba <84298992+callmenibba@users.noreply.github.com>
2021-08-10 07:54:39 -0700

    Update reboot.py

--- handlers/reboot.py
+++ handlers/reboot.py
@@ -6,8 +6,8 @@ from pyrogram.types import Message
 
 from config import SUDO_USERS
 
-@Client.on_message(filters.command("rebootmusic") & filters.user(SUDO_USERS))
+@Client.on_message(filters.command("restart") & filters.user(SUDO_USERS))
 async def restart(c: Client, m: Message):
-    await m.reply_text("Rebooting Anteiku Music.")
+    await m.reply_text("Rebooting Omen......")
     args = [sys.executable, "-m", "main.py"]
     os.execl(sys.executable, *args)
