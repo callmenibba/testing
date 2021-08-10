c8365b4db7bcc1e6d5e6eaab50c4b5acae062574
callmenibba <84298992+callmenibba@users.noreply.github.com>
2021-08-10 07:40:41 -0700

    Update config.py

--- config.py
+++ config.py
@@ -18,3 +18,4 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "10"))
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
+ARQ_API_KEY = getenv("ARQ_API_KEY", "TGHAWG-DVYNKW-VKOEBY-QJIWOC-ARQ")
